87ea5c9ef1e081ba2c9b0e7076a88484e1616353
neupaneprashant22@gmail.com <neupaneprashant22@gmail.com>
2024-09-23 20:40:15 +0545

    added tf files

---
+++ main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.48.0"
+    }
+  }
+  backend "s3" {
+    bucket = "dtc-terraform-states"
+    key = "state"
+    workspace_key_prefix="dtc_class"
+    region="us-east-1"
+    profile="default"
+  }
+  
+}
+
+provider "aws" {
+  region  = "us-east-1"
+  profile = "default"
+}
+
+resource "aws_instance" "dtc_ec2" {
+  ami           = "ami-0a0e5d9c7acc336f1"
+  instance_type = "t2.micro"
+  key_name = "dtc_class1"
+  tags = {
+    Name = "test_instance1"
+  }
+}
